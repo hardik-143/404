162b68f54f157b0f25303300af5ba6c86a6f79d4
Hardik <dhardik1430@gmail.com>
2023-12-31 17:31:33 +0530

    first commit

---
+++ skatesite/tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    theme: {
+      extend: {
+        colors: {
+          primary: "#DE623A",
+          secondary: "#DFDFDF",
+          dark: "#1C1C1C",
+          black: "#242424",
+        },
+        fontFamily: {
+          montserrat: ["Montserrat", "sans-serif"],
+          anton: ["Anton", "sans-serif"],
+        },
+      },
+    },
+  };
