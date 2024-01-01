577f474393372d32ff9ec8961cd82da286e3d9c1
Hardik <dhardik1430@gmail.com>
2024-01-01 22:43:30 +0530

    responsive added for skatesite

---
+++ shadowed/tailwind.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+    theme: {
+        extend: {
+          colors: {
+            secondary: "#DFDFDF",
+            dark: {
+              DEFAULT: "rgb(28,28,28)",
+              100: "rgb(28,28,28,0.1)",
+              200: "rgb(28,28,28,0.2)",
+              300: "rgb(28,28,28,0.3)",
+              400: "rgb(28,28,28,0.4)",
+              500: "rgb(28,28,28,0.5)",
+              600: "rgb(28,28,28,0.6)",
+              700: "rgb(28,28,28,0.7)",
+              800: "rgb(28,28,28,0.8)",
+              900: "rgb(28,28,28,0.9)",
+            },
+            black: "#242424",
+          },
+          fontFamily: {
+            cabin: ["Cabin", "sans-serif"],
+            questrial: ["Questrial", "sans-serif"],
+          },
+        },
+      },
+    };
